39543d571df6c8dd3683425773091e241317aee3
Michael Akakpo <michael.akakpo@nomadiq.online>
2024-05-11 10:30:39 +0100

    GetNewsArticleItemDetailUseCaseTest updated to use Fakes and Mocks and to handle success, error and network error cases.

--- app/src/test/java/com/nomadiq/finnews/domain/usecase/GetNewsArticleItemDetailUseCaseTest.kt
+++ app/src/test/java/com/nomadiq/finnews/domain/usecase/GetNewsArticleItemDetailUseCaseTest.kt
@@ -1,17 +1,30 @@
 package com.nomadiq.finnews.domain.usecase
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.nomadiq.finnews.data.network.connectivity.ConnectivityMonitor
+import com.nomadiq.finnews.data.network.connectivity.FakeConnectivityMonitor
+import com.nomadiq.finnews.domain.mapper.NewsArticleItemDetailResult
 import com.nomadiq.finnews.domain.repository.NewsArticleFeedRepository
+import com.nomadiq.finnews.utils.CoroutineTestRule
+import com.nomadiq.finnews.utils.TestConstants
+import com.nomadiq.finnews.utils.TestConstants.API_URL
+import com.nomadiq.finnews.utils.TestConstants.NETWORK_ERROR_MESSAGE
+import com.nomadiq.finnews.utils.TestConstants.UNKNOWN_ERROR
+import com.nomadiq.finnews.utils.article
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
-import utils.CoroutineTestRule
 
 /**
  *  @author Michael Akakpo
@@ -20,39 +33,80 @@ import utils.CoroutineTestRule
  *
  */
 
-@RunWith(MockitoJUnitRunner::class)
+@RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class GetNewsArticleItemDetailUseCaseTest {
 
-    private val dataRepository = mockk<NewsArticleFeedRepository>()
-
+    private val testDispatcher = StandardTestDispatcher()
+    private lateinit var repository: NewsArticleFeedRepository
+    private lateinit var connectivityMonitor: ConnectivityMonitor
     private lateinit var usecase: GetNewsArticleItemDetailUseCase
 
     @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule() // for the livedata
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @get:Rule
     val mainDispatcherRule =
-        CoroutineTestRule() // to replace the main dispatcher of the view model
+        CoroutineTestRule()
 
 
     @Before
     fun setup() {
         MockKAnnotations.init(this)
+        connectivityMonitor = FakeConnectivityMonitor()
+        repository = mockk<NewsArticleFeedRepository>(relaxed = true)
+        usecase = GetNewsArticleItemDetailUseCase(repository, connectivityMonitor)
     }
 
     @Test
-    fun `initialize then fetch news articles usecase succeeded`() = runTest {
+    fun `open fetched news item detail usecase succeeded`() = runTest(testDispatcher) {
+        //  Given
+        val expectedResult = NewsArticleItemDetailResult.Success(
+            article
+        )
+        coEvery { repository.fetchNewsArticleItemDetail(API_URL) }.returns(expectedResult)
 
+        // when
+        val result = usecase.invoke(API_URL).first()
+        coVerify { repository.fetchNewsArticleItemDetail(API_URL) }
+
+        // then
+        Truth.assertThat(result).isEqualTo(expectedResult)
+        Truth.assertThat(expectedResult.item.title).isEqualTo(TestConstants.TITLE_TWO)
+        Truth.assertThat(expectedResult.item.date).isEqualTo(TestConstants.SUBTITLE_TWO)
+        Truth.assertThat(expectedResult.item.imgUrl).isEqualTo(TestConstants.IMG_URL)
+        Truth.assertThat(expectedResult.item.body).isEqualTo(TestConstants.BODY)
     }
 
     @Test
-    fun `initialize then fetch dog breeds usecase failed Empty list`() = runTest {
+    fun `open fetched news item detail usecase failed error`() = runTest(testDispatcher) {
+        //  Given
+        val expectedResult = NewsArticleItemDetailResult.Error(
+            UNKNOWN_ERROR
+        )
+        coEvery { repository.fetchNewsArticleItemDetail(API_URL) }.returns(expectedResult)
 
+        // when
+        val result = usecase.invoke(API_URL).first()
+        coVerify { repository.fetchNewsArticleItemDetail(API_URL) }
+
+        // then
+        Truth.assertThat(result).isEqualTo(expectedResult)
     }
 
     @Test
-    fun `initialize then fetch news articles usecase failed Error`() = runTest {
+    fun `open fetched news item detail usecase network Error`() = runTest(testDispatcher) {
+        //  Given
+        val expectedResult = NewsArticleItemDetailResult.NetworkError(
+            NETWORK_ERROR_MESSAGE
+        )
+        coEvery { repository.fetchNewsArticleItemDetail(API_URL) }.returns(expectedResult)
 
+        // when
+        val result = usecase.invoke(API_URL).first()
+        coVerify { repository.fetchNewsArticleItemDetail(API_URL) }
+
+        // then
+        Truth.assertThat(result).isEqualTo(expectedResult)
     }
 }
