63b948644db8d0cee82e214c99cf4746268de97f
Michael Akakpo <michael.akakpo@nomadiq.online>
2024-06-23 22:11:36 +0100

    Updated AGP to latest stable release. All tests passing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.dagger.hilt.android")
-    kotlin("kapt") version "1.9.22"
+    kotlin("kapt") version "1.9.24"
 }
 
 android {
@@ -29,7 +29,8 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            val apiKey = "\"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\""
+            val apiKey = "\"b77f75d9-492b-4d80-bab3-088e00fd5f7b\""
+            //val apiKey = "\"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\""
             val baseURL = "https://content.guardianapis.com/"
             buildConfigField("String", "BASE_URL", "\"$baseURL\"")
             buildConfigField("String", "API_KEY", apiKey)
@@ -37,7 +38,8 @@ android {
 
         debug {
             isMinifyEnabled = false
-            val apiKey = "\"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\""
+            val apiKey = "\"b77f75d9-492b-4d80-bab3-088e00fd5f7b\""
+            // val apiKey = "\"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\""
             val baseURL = "https://content.guardianapis.com/"
             buildConfigField("String", "BASE_URL", "\"$baseURL\"")
             buildConfigField("String", "API_KEY", apiKey)
@@ -55,13 +57,20 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.9"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
     kapt {
         // Allow references to generated code
         correctErrorTypes = true
@@ -91,14 +100,12 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     // Material 3 - theming
     implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
     // Optional - Integration with ViewModels
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
     // Optional - Integration with LiveData
     implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
-
-    // Navigation with Compose
-    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // AppCompat libs
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -127,8 +134,6 @@ dependencies {
     // Dependency Injection (Hilt)
     implementation("com.google.dagger:hilt-android:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.test.ext:junit-ktx:1.1.5")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
 
     // Hilt - For instrumentation tests
@@ -142,7 +147,6 @@ dependencies {
     // Compose preview
     implementation("androidx.compose.ui:ui-tooling-preview-android:1.6.8")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // For Coroutines testing
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
@@ -152,30 +156,49 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("junit:junit:4.13.2")
 
+    // JUnit Test Runner
+    implementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
     // Mockito
     testImplementation("org.mockito:mockito-core:5.12.0")
 
     // Google Truth - Assertions
     testImplementation("com.google.truth:truth:1.4.2")
-    testImplementation("com.google.truth:truth:1.4.2")
 
     // For Mockk
     testImplementation("io.mockk:mockk:1.13.11")
 
-    // Espresso
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Robolectric
+    testImplementation("org.robolectric:robolectric:4.12.2")
+    testImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("org.robolectric:robolectric:4.12.2")
 
-    // Navigation Testing
+    // Compose Test rules and transitive dependencies:
+    testImplementation("androidx.compose.ui:ui-test-junit4:1.6.8")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.8")
+    // Needed for createComposeRule(), but not for createAndroidComposeRule<YourActivity>():
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
+
+    // Instrumentation test runner
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+
+    // Optional -- UI testing with Espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Optional -- UI testing with Compose
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.8")
+    // Optional -- UI testing with UI Automator
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
+
+    // Navigation with Compose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.navigation:navigation-testing:2.7.7")
 
-    // Junit Testing
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    // Robolectric
-    implementation("org.robolectric:robolectric:4.12.2")
-
-    // Testing Compose
+    // Compose Tests
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test:1.6.8")
 
     // Timber - Logging
     implementation("com.jakewharton.timber:timber:5.0.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    kotlin("kapt") version "1.9.22"
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    kotlin("kapt") version "1.9.24"
     id("com.google.dagger.hilt.android") version "2.51" apply false
 }
 
