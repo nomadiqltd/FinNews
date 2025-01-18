cfe0c9263a29210870ab2c2a5552f07dce4868a4
Michael Akakpo <michael.akakpo@nomadiq.online>
2025-01-18 00:57:59 +0000

    Downgrade the targetSDK from 35 to 34. Unit Tests now pass. Updated gradle, build config.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.nomadiq.finnews"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
